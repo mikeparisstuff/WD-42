0e1c36074d812d09ce5124b8be42a66ad00b7351
Michael Paris <mlparis92@gmail.com>
2014-04-27 21:24:20 -0400

    cleanup

--- rustic.rs
+++ rustic.rs
@@ -10,55 +10,50 @@ use std::Vec;
 use collections::HashMap;
 mod application;
 
+fn indexGet(req: &Request, res: &mut ResponseWriter) {
+    println!("Hello get!");
+    res.status = Ok;
+    res.sendFile(~"index.html");
+    // res.write(bytes!("Hello World!"));
+}
+fn indexPost(req: &Request, res: &mut ResponseWriter) {
+    println!("Hello post!");
+}
+
+fn noFileGet(req: &Request, res: &mut ResponseWriter) {
+    res.status = Ok;
+    res.sendFile(~"nobedere.html");
+}
+
+fn authenticate(req: &Request) {
+    let allowedUsers : [~str, ..4] = [~"mlp5ab", ~"ag7bf", ~"bp5xj", ~"nal3vm"];
+    println!("Length of headers: {}", req.headers.extensions.len());
+    match req.headers.extensions.find(&~"Authorization") {
+        Some(user) => {
+            if allowedUsers.contains(user) {
+                println!("Successfully authenticated user: {}", user);
+            } else {
+                println!("Could not authenticate user: {}", user);
+            }
+        },
+        None => {
+            println("Could not find Authorization in headers");
+        }
+    }
+}
+
+fn checkAuth(req: &Request) {
+    println!("This request is authenticated: {}", req.is_authenticated);
+}
+
+
 fn main() {
 
     let mut app: application::App = application::App::new();
 
     // TODO: Use actual request and response objects
-    fn indexGet(req: &Request, res: &mut ResponseWriter) {
-        println!("Hello get!");
-        res.status = Ok;
-        res.sendFile(~"index.html");
-        // res.write(bytes!("Hello World!"));
-    }
-    fn indexPost(req: &Request, res: &mut ResponseWriter) {
-        println!("Hello post!");
-    }
-
-    fn noFileGet(req: &Request, res: &mut ResponseWriter) {
-        res.status = Ok;
-        res.sendFile(~"nobedere.html");
-    }
-
-    fn authenticate(req: &Request) {
-        let allowedUsers : [~str, ..4] = [~"mlp5ab", ~"ag7bf", ~"bp5xj", ~"nal3vm"];
-        println!("Length of headers: {}", req.headers.extensions.len());
-        match req.headers.extensions.find(&~"Authorization") {
-            Some(user) => {
-                if allowedUsers.contains(user) {
-                    println!("Successfully authenticated user: {}", user);
-                } else {
-                    println!("Could not authenticate user: {}", user);
-                }
-            },
-            None => {
-                println("Could not find Authorization in headers");
-            }
-        }
-    }
-
-    fn checkAuth(req: &Request) {
-        println!("This request is authenticated: {}", req.is_authenticated);
-    }
 
     app.set_public_dir(~"./public");
-
-    // fn indexPut(req: &Request, res: &mut ResponseWriter) {
-    //     println!("Hello put!");
-    // }
-    // fn indexDelete(req: &Request, res: &mut ResponseWriter) {
-    //     println!("Hello delete!");
-    // }
 
     app.get(~"/", indexGet);
     app.post(~"/", indexPost);
